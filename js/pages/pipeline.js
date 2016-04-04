0bf984e86b2be6c27b2a1e9e298a3d22b22c880f
lroth <lukasz.roth@gmail.com>
2016-04-04 22:43:09 +0200

    pipeline

--- js/pages/pipeline.js
+++ js/pages/pipeline.js
@@ -6,17 +6,18 @@ export class Pipeline extends Component {
         return <div className="container pipeline">
             <div className="col-md-4">
                 <h4>Lead</h4>
+                <h5>1 lead worth: $25,000</h5>
                 <div className="card text-xs-center">
                   <div className="card-header">
-                    Featured
+                    Bostons new page module
                   </div>
                   <div className="card-block">
-                    <h4 className="card-title">Special title treatment</h4>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text">
+                        With supporting text below as a natural lead-in to additional content.
+                    </p>
                   </div>
                   <div className="card-footer text-muted">
-                    2 days ago
+                    Luxus | 10/04/2016 | Pawel
                   </div>
                 </div>
             </div>
